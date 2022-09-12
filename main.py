0034c0ce6f42796f19222d5c61d051a1601b01b7
Lasantha <lasantha1130@gmail.com>
2022-09-12 13:13:26 +0530

    add cafes to website from db

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import re
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
+
+def get_cafes():
+    conn = sqlite3.connect("cafes.db")
+    cur = conn.cursor()
+    res = cur.execute("SELECT * FROM cafe")
+    return res.fetchall()
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    cafes = get_cafes()
+    return render_template("index.html", cafes=cafes)
 
 
 if __name__ == "__main__":
